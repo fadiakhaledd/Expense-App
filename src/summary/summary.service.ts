edb9a3976679f71636b06118dea5e10f238bf2fa
Fadia Khaled <Fadiaabdelhadyy@gmail.com>
2023-06-19 17:03:03 +0300

    Final

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import { ReportType } from 'src/data';
+import { ReportService } from 'src/report/report.service';
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+
+  calculateSummary() {
+    const incomeReports = this.reportService.getAllReports(ReportType.INCOME);
+    const expenseReports = this.reportService.getAllReports(ReportType.EXPENSE);
+
+    const totalExpense = expenseReports.reduce(
+      (sum, report) => sum + report.amount,
+      0,
+    );
+    const totalIncome = incomeReports.reduce(
+      (sum, report) => sum + report.amount,
+      0,
+    );
+
+    return {
+      totalExpense,
+      totalIncome,
+      netIncome: totalIncome - totalExpense,
+    };
+  }
+}
